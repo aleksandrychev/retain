c74d55ce87a5ba4c7409c3cd16a620243dd2634e
Igor Aleksandrychev <i.aleksandrychev@relevant.software>
2016-03-14 20:22:30 +0200

    add new function

--- basic/controllers/UploadController.php
+++ basic/controllers/UploadController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace app\controllers;
+use app\models\logic\AlchemyAPI;
 use app\models\logic\Pdf2htmlExModel;
 use yii\web\UploadedFile;
 use Yii;
@@ -20,9 +21,15 @@ class UploadController extends \yii\web\Controller
             $model->pdf = UploadedFile::getInstance($model, 'pdf');
             $pdfName = $model->upload();
             if ($pdfName) {
-                $pdfToHtml = new Pdf2htmlExModel($pdfName);
+                $pdfToHtml = new Pdf2htmlExModel($pdfName. '.pdf');
                 $pdfToHtml->pdfToHtmlConvertion();
-               var_dump($pdfName);exit;
+                $html = strip_tags(file_get_contents(__DIR__ . '/../web/uploads/html/' . $pdfName . '.html'));
+
+                $api = new AlchemyAPI();
+                $apires = $api->textExtractDates($html);
+                var_dump($apires);exit;
+
+                exit;
                 return;
             }
         }
--- basic/models/logic/AlchemyAPI.php
+++ basic/models/logic/AlchemyAPI.php
@@ -11,5 +11,54 @@ namespace app\models\logic;
 
 class AlchemyAPI
 {
+    private $apiKey = '7ba9369c80e73f358e8d6ad4ac9d8f6cf796f9ec';
+    private $dateApiUrl = 'http://gateway-a.watsonplatform.net/calls/text/TextExtractDates';
+    private $entityApiUrl = 'http://gateway-a.watsonplatform.net/calls/text/TextExtractDates';
+
+    public function setApiKey($key){
+        $this->apiKey = $key;
+    }
+
+    public function textGetRankedNamedEntities($text){
+        $data = [
+            'apikey' => $this->apiKey,
+            'outputMode'=> 'json',
+            'showSourceText'=> 1,
+            'text'=>$text
+        ];
+
+        return $this->callAPI($this->entityApiUrl, $data);
+    }
+
+    public function textExtractDates($text){
+
+        $data = [
+            'apikey' => $this->apiKey,
+            'outputMode'=> 'json',
+            'showSourceText'=> 1,
+            'text'=>$text
+        ];
+
+       return $this->callAPI($this->dateApiUrl, $data);
+    }
+
+
+    private function callAPI($url, $data)
+    {
+        $curl = curl_init();
+
+
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $result;
+    }
 
 }
