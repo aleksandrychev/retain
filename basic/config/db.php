9996f0b99bfbd2bf54e3f2f41809cfb4634bded8
Igor Aleksandrychev <i.aleksandrychev@relevant.software>
2016-03-15 10:44:55 +0200

    upd gitignore

--- basic/config/db.php
+++ basic/config/db.php
@@ -1,9 +1,14 @@
 <?php
 
-return [
+$db = [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=pdf2html',
     'username' => 'root',
     'password' => '111111',
     'charset' => 'utf8',
 ];
+
+@include_once("db-local.php");
+
+
+return $db;
