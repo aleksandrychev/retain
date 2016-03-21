d36935157ee0270eaaedbc88b397fc885a89c10b
Igor Aleksandrychev <i.aleksandrychev@relevant.software>
2016-03-21 18:04:06 +0200

    upd regex

--- basic/helpers/AppHelper.php
+++ basic/helpers/AppHelper.php
@@ -28,7 +28,7 @@ class AppHelper
         $phrase= self::clearHtml($phrase);
         $text = file_get_contents(__DIR__ . '/../web/uploads/html/' . $htmlFileName);
         $text = strip_tags(AppHelper::clearHtml($text));
-        $re = '/(?<=[.!?”•;]|[.!?”•;][\'"])(?=[A-Z"\'])/';
+        $re = '/(?<=[.!?”•;]|[.!?”•;][\'"])(?=[A-Z"\']|[\s+])/';
         $sentences = preg_split($re, $text, -1, PREG_SPLIT_NO_EMPTY);
 
         if (array_key_exists($phrase, self::$tempSent)) {
