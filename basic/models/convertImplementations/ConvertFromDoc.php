ce258ce1acd01c25754afdc109a911f2543ae9da
Igor Aleksandrychev <i.aleksandrychev@relevant.software>
2016-05-04 17:54:38 +0300

    change exec commands

--- basic/models/convertImplementations/ConvertFromDoc.php
+++ basic/models/convertImplementations/ConvertFromDoc.php
@@ -16,8 +16,10 @@ class ConvertFromDoc extends AbstractConverterBase
     {
         $uploadsDir = __DIR__ . '/../../web/uploads';
         $command = "cd $uploadsDir;";
-        $command .= 'sudo /usr/bin/unoconv -o "pdf/'. $this->docID .'.pdf" -fpdf doc/'. $this->docID .'.doc;';
+        putenv('PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/opt/node/bin:/usr/lib64/libreoffice');
+        $command .= 'unoconv --listener 2>&1;  /usr/bin/unoconv  -o "pdf/'. $this->docID .'.pdf" -fpdf doc/'. $this->docID .'.doc;';
         $command .= 'pdf2htmlEX  --dest-dir ./html ./pdf/' . $this->docID . '.pdf';
+
         exec($command, $output, $return_var);
 
     }
