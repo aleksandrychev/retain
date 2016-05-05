cc0260383b8cc6a5979086cfbde0363f2add4fbe
Igor Aleksandrychev <i.aleksandrychev@relevant.software>
2016-05-05 14:45:25 +0300

    change migration

--- basic/migrations/m160505_112213_add_uiid_column_to_dociments.php
+++ basic/migrations/m160505_112213_add_uiid_column_to_dociments.php
@@ -7,10 +7,12 @@ class m160505_112213_add_uiid_column_to_dociments extends Migration
     public function up()
     {
         $this->addColumn('documents','uuid','varchar(50)');
+        $this->addColumn('sentences_plus_hl','send_to_final','varchar(50)');
     }
 
     public function down()
     {
         $this->dropColumn('documents','uuid');
+        $this->dropColumn('sentences_plus_hl','send_to_final');
     }
 }
