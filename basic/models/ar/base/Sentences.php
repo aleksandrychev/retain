d3fcb7054ef41df04e186412a2f0aaccf2978403
igor <leopolis.best@gmail.com>
2016-08-16 13:31:20 +0300

    delete tag_id

--- basic/models/ar/base/Sentences.php
+++ basic/models/ar/base/Sentences.php
@@ -9,9 +9,7 @@ use Yii;
  *
  * @property integer $id
  * @property integer $doc_id
- * @property integer $tag_id
  * @property integer $user_id
- * @property integer $project_id
  * @property string $entity_type
  * @property string $entity
  * @property string $sentence
@@ -32,12 +30,11 @@ class Sentences extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['doc_id', 'user_id', 'project_id'], 'integer'],
+            [['doc_id', 'user_id'], 'integer'],
             [['sentence'], 'string'],
             [['entity_type'], 'string', 'max' => 200],
             [['entity'], 'string', 'max' => 400],
             [['doc_id'], 'exist', 'skipOnError' => true, 'targetClass' => Documents::className(), 'targetAttribute' => ['doc_id' => 'id']],
-            [['project_id'], 'exist', 'skipOnError' => true, 'targetClass' => Projects::className(), 'targetAttribute' => ['project_id' => 'id']],
             [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
         ];
     }
@@ -51,7 +48,6 @@ class Sentences extends \yii\db\ActiveRecord
             'id' => 'ID',
             'doc_id' => 'Doc ID',
             'user_id' => 'User ID',
-            'project_id' => 'Project ID',
             'entity_type' => 'Entity Type',
             'entity' => 'Entity',
             'sentence' => 'Sentence',
